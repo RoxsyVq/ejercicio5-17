157220db54e2a046d591a0c0856b271ce722cff2
paola <lizbethochoa163@gmail.com>
2016-12-27 12:06:16 -0500

    segundo avance

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
 function habla()
 {
-	
+	var vhora= document.getElementById("hora").value;
 	var vhabla = document.getElementById("habla");
     var salida = document.getElementById("salida");
 
-	if(vhabla.checked)
+	if(vhora<7 || vhora>20 && (vhabla.checked))
 		salida.innerHTML = 1;
 	else
 		salida.innerHTML = 0; 
 }
 
-function calcular(){
-    var vhora= document.getElementById("hora").value;
-    var vhabla= document.getElementById("habla").checked;
-    var salida = document.getElementById("salida");
-    vhora>=23;
-    
-    if( (vhora < 7 || vhora > 20) && vhabla.checked){
-        salida.innerHTML = 1;
-    }
-    else{
-        salida.innerHTML = 0;    }
-}
+
